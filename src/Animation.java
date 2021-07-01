b65efec0b576e7b292cb5ee6694cf99c7e42938a
cdklas1230 <86804577+cdklas1230@users.noreply.github.com>
2021-07-01 14:36:33 -0700

    Add files via upload

---
+++ src/Animation.java
@@ -0,0 +1,39 @@
+import controller.AnimationController;
+
+/**
+ * Create an animation class to run the animation in terminal.
+ *
+ */
+public class Animation {
+
+  /**
+   * Main function to run the animation.
+   * @param args input from the terminal
+   */
+  public static void main(String[] args) {
+    int speed = 20;
+    String in = "./src/test.txt";
+    String out = "";
+    String viewType = "text";
+
+    for (int i = 0; i < args.length; i++) {
+      if (args[i].equals("-in")) {
+        in = args[i + 1];
+      } else if (args[i].equals("-out")) {
+        out = args[i + 1];
+      } else if (args[i].equals("-speed")) {
+        speed = Integer.parseInt(args[i + 1]);
+      } else if (args[i].equals("-view")) {
+        viewType = args[i + 1];
+      } else {
+        throw new IllegalArgumentException("InValid arguments");
+      }
+
+      i++;
+    }
+
+    AnimationController ac = new AnimationController(speed, viewType, in, out);
+    ac.play();
+  }
+
+}
---
+++ src/view/IView.java
@@ -0,0 +1,14 @@
+package view;
+
+/**
+ * Represents the interface for all types of view.
+ *
+ */
+public interface IView {
+  /**
+   * The display method for all types of view.
+   * @throws Exception Different views should throw exceptions accordingly
+   */
+  void display() throws Exception;
+  
+}
